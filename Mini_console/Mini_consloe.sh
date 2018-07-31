057a1418f052b01bdb27628ec4c19e789522534e
Zahid4960 <zahidcse4960@gmail.com>
2018-07-31 12:16:35 +0600

    Created header and menu

--- Mini_console/Mini_consloe.sh
+++ Mini_console/Mini_consloe.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+#starting of header part
+clear
+echo "                                                              Mini Console"
+echo "                                                  Developed by Department of CSE DIU"
+#Closing of header part
+
+echo
+echo
+echo
+
+#Starting of menu
+echo "                                                         1. Date"
+echo "                                                         2. Time"
+echo "                                                         3. Calendar"
+echo "                                                         4. User"
+echo "                                                         5. IP Address"
+echo "                                                         6. Contacts"
+echo "                                                         7. Notebook"
+echo "                                                         8. Calculator"
+echo "                                                         9. Music"
+echo "                                                        10. Videos"
+echo "                                                        11. About us"
+echo "                                                        12. Exit"
+#Closing of menu
+
+echo
+echo
+echo -ne "                                                  Please enter your choice:"
+read choice
